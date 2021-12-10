432db090138f4e30bdec54eb641a66cab0464f31
Pablo Zuniga <ddjpablito@hotmail.com>
2021-12-10 16:10:34 +0000

    Primer Commit

---
+++ bool.py
@@ -0,0 +1,10 @@
+a = 5 > 3
+print('5 > 3 es: ' + str(a))
+b = 5 < 3
+print('5 < 3 es: ' + str(b))
+c = 5 == 2 + 3
+print('5 == 2 + 3 es: ' + str(c))
+
+print('5 > 3 es:', a)
+print('5 < 3 es:', b)
+print('5 == 2 + 3 es:', c)
---
+++ bucle.py
@@ -0,0 +1,9 @@
+a = input('Ingrese un valor para el número a: ')
+b = input('Ingrese un valor para el número b: ')
+a = int(a)
+b = int(b)
+print('Los números entre a y b son: ')
+numero = a + 1
+while numero < b:
+    print(numero)
+    numero = numero + 1
